e0b3bb45c7a21eb3ad290d84e644f28f192b7c8d
alanrychert <alanrychert@gmail.com>
2020-10-08 19:36:06 -0300

    Base de datos lista para entrega 1
    (Igual podriamos revisar por las dudas si quedó prolijo)

---
+++ borrar.sql
@@ -0,0 +1,10 @@
+drop database parquimetros;
+
+drop user admin@localhost;
+
+drop user inspector@'%';
+
+drop user venta@'%';
+
+flush privileges;
+
---
+++ parquimetros.sql
@@ -0,0 +1,239 @@
+#Archivo batch para la creacion de la 
+#Base de datos del práctico de SQL
+
+# Creo de la Base de Datos
+CREATE DATABASE parquimetros;
+
+# selecciono la base de datos sobre la cual voy a hacer modificaciones
+USE parquimetros;
+
+#-------------------------------------------------------------------------
+#Creacion Tablas para las entidades
+
+CREATE TABLE Ubicaciones(
+	altura SMALLINT UNSIGNED NOT NULL,
+	calle VARCHAR(45) NOT NULL,
+	tarifa DECIMAL(5,2) UNSIGNED NOT NULL,
+
+
+	CONSTRAINT pk_ubicaciones
+		PRIMARY KEY (altura,calle)
+)ENGINE=InnoDB;
+
+CREATE TABLE Parquimetros(
+ id_parq INT UNSIGNED NOT NULL,
+ numero INT UNSIGNED NOT NULL,
+ calle VARCHAR(45) NOT NULL,
+ altura SMALLINT UNSIGNED NOT NULL,
+
+ 
+ CONSTRAINT pk_parquimetros
+ PRIMARY KEY (id_parq),
+
+ CONSTRAINT fk_ubicacion_parquimetros
+ FOREIGN KEY (altura,calle) REFERENCES Ubicaciones (altura,calle)
+   ON DELETE RESTRICT ON UPDATE CASCADE
+
+) ENGINE=InnoDB;
+
+CREATE TABLE Inspectores(
+	legajo SMALLINT UNSIGNED NOT NULL,
+	dni SMALLINT UNSIGNED NOT NULL,
+	nombre VARCHAR(45) NOT NULL,
+	password CHAR(32) NOT NULL,
+	apellido VARCHAR(45) NOT NULL,
+
+
+	CONSTRAINT pk_inspectores
+		PRIMARY KEY (legajo)
+)ENGINE=InnoDB;
+
+CREATE TABLE Asociado_con (
+ id_asociado_con INT UNSIGNED NOT NULL AUTO_INCREMENT,
+ legajo SMALLINT UNSIGNED NOT NULL,
+ calle VARCHAR(45) NOT NULL,
+ altura SMALLINT UNSIGNED NOT NULL,
+ dia enum('do','lu','ma','mi','ju','vi','sa') NOT NULL,
+ turno enum('m','t') NOT NULL,
+
+ 
+ CONSTRAINT pk_id_asociado_con
+ PRIMARY KEY (id_asociado_con),
+
+ CONSTRAINT fk_legajo_asoc_con
+ FOREIGN KEY (legajo) REFERENCES Inspectores(legajo)
+   ON DELETE RESTRICT ON UPDATE CASCADE,
+
+ CONSTRAINT fk_ubicacion
+ FOREIGN KEY (altura,calle) REFERENCES Ubicaciones(altura,calle)
+   ON DELETE RESTRICT ON UPDATE CASCADE
+
+) ENGINE=InnoDB;
+
+CREATE TABLE Conductores (
+ dni SMALLINT (45) UNSIGNED NOT NULL,
+ nombre VARCHAR(45) NOT NULL,
+ apellido VARCHAR(45) NOT NULL,
+ direccion VARCHAR(45) NOT NULL,
+ telefono VARCHAR(45) ,
+ registro SMALLINT(45) UNSIGNED NOT NULL,
+
+ 
+ CONSTRAINT pk_conductores
+ PRIMARY KEY (dni)
+) ENGINE=InnoDB;
+
+CREATE TABLE Automoviles (
+ marca VARCHAR(45) NOT NULL, 
+ modelo VARCHAR(45) NOT NULL,
+ patente VARCHAR(6) NOT NULL, 
+ color VARCHAR(45) NOT NULL,
+ dni SMALLINT (45) UNSIGNED NOT NULL,
+
+ CONSTRAINT pk_automoviles
+ PRIMARY KEY (patente),
+
+ CONSTRAINT FK_automoviles
+ FOREIGN KEY (dni) REFERENCES Conductores(dni)
+   ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Tipos_tarjeta (
+ tipo VARCHAR (45) NOT NULL,
+ descuento DECIMAL (3,2) UNSIGNED NOT NULL,
+
+ CONSTRAINT tipos_tarjeta
+ PRIMARY KEY (tipo)
+) ENGINE=InnoDB;
+
+CREATE TABLE Tarjetas(
+	id_tarjeta SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	saldo DECIMAL(5,2) NOT NULL,
+	tipo VARCHAR(45) NOT NULL,
+	patente CHAR(6) NOT NULL,
+
+	CONSTRAINT pk_tarjeta
+		PRIMARY KEY (id_tarjeta),
+
+	CONSTRAINT FK_tipo
+	FOREIGN KEY (tipo) REFERENCES Tipos_tarjeta(tipo)
+		ON DELETE RESTRICT ON UPDATE CASCADE,
+
+	CONSTRAINT FK_tarjeta
+	FOREIGN KEY (patente) REFERENCES Automoviles(patente)
+   		ON DELETE RESTRICT ON UPDATE CASCADE
+
+)ENGINE=InnoDB;
+
+
+#----------------------------------------------------------alan
+
+CREATE TABLE Multa (
+ numero INT UNSIGNED NOT NULL AUTO_INCREMENT,
+ fecha DATE NOT NULL,
+ hora TIME NOT NULL,
+ patente VARCHAR(6) NOT NULL, 
+ id_asociado_con INT UNSIGNED NOT NULL,
+ 
+ CONSTRAINT pk_multa
+ PRIMARY KEY (numero),
+
+ CONSTRAINT FK_patente 
+ FOREIGN KEY (patente) REFERENCES Automoviles (patente) 
+   ON DELETE RESTRICT ON UPDATE CASCADE,
+
+ CONSTRAINT FK_asociado_con 
+ FOREIGN KEY (id_asociado_con) REFERENCES Asociado_con (id_asociado_con) 
+   ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+
+CREATE TABLE Accede (
+ legajo SMALLINT UNSIGNED NOT NULL,
+ id_parq INT UNSIGNED NOT NULL,
+ fecha DATE NOT NULL,
+ hora TIME NOT NULL,
+
+ 
+ CONSTRAINT pk_accede
+ PRIMARY KEY (id_parq,fecha,hora),
+
+ CONSTRAINT fk_id_parq
+ FOREIGN KEY (id_parq) REFERENCES Parquimetros(id_parq)
+   ON DELETE RESTRICT ON UPDATE CASCADE,
+
+ CONSTRAINT fk_legajo
+ FOREIGN KEY (legajo) REFERENCES Inspectores (legajo)
+   ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Estacionamientos (
+ id_tarjeta SMALLINT UNSIGNED NOT NULL,
+ id_parq INT UNSIGNED NOT NULL,
+ fecha_ent DATE NOT NULL,
+ hora_ent TIME NOT NULL,
+ fecha_sal DATE,
+ hora_sal TIME,
+
+ 
+ CONSTRAINT pk_Estacionamientos
+ PRIMARY KEY (id_parq,fecha_ent,hora_ent),
+
+ CONSTRAINT fk_id_estacionamientos
+ FOREIGN KEY (id_parq) REFERENCES Parquimetros(id_parq)
+   ON DELETE RESTRICT ON UPDATE CASCADE,
+
+ CONSTRAINT fk_id_tarjeta
+ FOREIGN KEY (id_tarjeta) REFERENCES Tarjetas(id_tarjeta)
+   ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+#-------------------------------------------------------------------------
+# Creacion de vistas 
+# estacionados = Permite ver la calle, altura y las patentes de los autos que tienen un estacionamiento "abierto" en cada ubicación
+
+ CREATE VIEW estacionados AS 
+ SELECT calle, altura, patente 
+ FROM (parquimetros.parquimetros NATURAL JOIN estacionamientos NATURAL JOIN tarjetas)
+ WHERE fecha_ent <> NULL and hora_ent <> NULL and fecha_sal = NULL and hora_sal = NULL;
+
+#-------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------
+# Creacion de usuarios 
+
+# Creo el usuario admin con contraseña "admin" que únicamente se puede conectar desde la computadora donde
+# se encuentra el servidor de MySQL
+ 
+ CREATE USER 'admin'@'localhost'  IDENTIFIED BY 'admin'; 
+
+# luego le otorgo todos los privilegios sobre la base de datos y también el privilegio de 
+# otorgar privilegios a otro usuario de la misma
+
+ GRANT ALL PRIVILEGES ON parquimetros.* TO 'admin'@'localhost' WITH GRANT OPTION;
+
+# Creo el usuario venta, que se puede conectar desde cualquier computadora
+
+ CREATE USER 'venta'@'%' IDENTIFIED BY 'venta';
+
+# Le otorgo permisos de inserción en la tabla tarjetas y de selección en la tabla tipos_tarjeta
+
+ GRANT INSERT ON parquimetros.tarjetas TO 'venta'@'%';
+ GRANT SELECT ON parquimetros.tipos_tarjeta TO 'venta'@'%';
+
+# Creo el usuario inspector, con la constraseña "inspector" que se puede conectar desde cualquier computadora
+
+CREATE USER 'inspector'@'%' IDENTIFIED BY 'inspector';
+
+# Le otorgo los permisos necesarios para validar número de legajo y password de un inspector,
+# Dado un parquímetro, obtener las patentes de los autom´oviles con un estacionamiento
+# abierto en la ubicación del parqu´ımetro,
+# cargar multas, y
+# registrar accesos a parquímetros.
+
+GRANT SELECT ON parquimetros.Parquimetros TO 'inspector'@'%';
+GRANT SELECT ON parquimetros.Inspectores TO 'inspector'@'%';
+GRANT SELECT ON parquimetros.Estacionados TO 'inspector'@'%';
+GRANT INSERT ON parquimetros.Multa TO 'inspector'@'%';
+GRANT INSERT ON parquimetros.Accede TO 'inspector'@'%';
