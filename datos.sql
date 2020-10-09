9e856f79047a2c04b0026fd7861f5ad0d0fbe926
alanrychert <alanrychert@gmail.com>
2020-10-09 19:41:42 -0300

    Create datos.sql

---
+++ datos.sql
@@ -0,0 +1,17 @@
+USE batallas;
+
+#Inserciones de inspectores (el legajo es de 1 solo digito, el dni de 2 digitos)
+#-------------------------------------------------------------------
+INSERT INTO inspectores VALUES(1,11,"Inspector1","uno","Ap1_I");
+INSERT INTO inspectores VALUES(2,22,"Inspector2","dos","Ap2_I");
+INSERT INTO inspectores VALUES(3,33,"Inspector3","tres","Ap3_I");
+INSERT INTO inspectores VALUES(4,44,"Inspector4","cuatro","Ap4_I");
+INSERT INTO inspectores VALUES(5,55,"Inspector5","cinco","Ap5_I");
+#--------------------------------------------------------------------
+
+#Inserciones de conductores(el dni es de 3 digitos, el nro de telefono de 4 digitos, y
+#el registro de 1 digito)
+#--------------------------------------------------------------------
+INSERT INTO conductores VALUES(111,"Conductor1","Ap1_C","dir1",1111,1);
+
+
