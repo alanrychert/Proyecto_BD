10611c7ceb56df84c196c3aa04740226995f6b0f
alanrychert <alanrychert@gmail.com>
2020-10-26 21:30:43 -0300

    VentanaInspector
    Agregamos la parte de la lista de patentes, el boton verificar, los labels donde se muestra la calle, altura y parquimetro de la ubicacion seleccionada desde la tabla

--- src/parquimetros/VentanaInspector.java
+++ src/parquimetros/VentanaInspector.java
@@ -1,6 +1,11 @@
 package parquimetros;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
@@ -11,7 +16,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -31,6 +40,14 @@ public class VentanaInspector extends javax.swing.JInternalFrame
    private JTextField txtNombre;
    private JTable tabla;
    private JLabel lblNombre;
+   private JPanel panelTablaFiltro;
+   private JPanel panelSeleccionados;
+   private JLabel lblCalleSelec;
+   private JLabel lblAlturaSelec;
+   private JLabel lblIdParqSelec;
+   private JButton btnVerificar;
+   private JPanel panelPatentes;
+   private DefaultListModel<String> l1;
    
    protected Connection conexionBD = null;
 
@@ -54,6 +71,26 @@ public class VentanaInspector extends javax.swing.JInternalFrame
          BorderLayout thisLayout = new BorderLayout();
          this.setVisible(true);
          getContentPane().setLayout(thisLayout);
+         pnlFiltroNombre = new JPanel();
+         getContentPane().add(pnlFiltroNombre, BorderLayout.NORTH);
+         {
+            lblNombre = new JLabel();
+            pnlFiltroNombre.add(lblNombre);
+            lblNombre.setText("Búsqueda incremental del nombre");
+         }
+         {
+            txtNombre = new JTextField();               
+            pnlFiltroNombre.add(txtNombre);
+            txtNombre.setColumns(30);
+            txtNombre.addCaretListener(new CaretListener() {
+               public void caretUpdate(CaretEvent evt) {
+                  txtNombreCaretUpdate(evt);
+               }
+            });
+         }
+         crearPanelTablaFiltro();
+         crearPanelPatentes();
+         crearPanelSeleccionados();
          this.addComponentListener(new ComponentAdapter() {
             public void componentHidden(ComponentEvent evt) {
                thisComponentHidden(evt);
@@ -62,91 +99,6 @@ public class VentanaInspector extends javax.swing.JInternalFrame
                thisComponentShown(evt);
             }
          });
-         {
-            pnlFiltroNombre = new JPanel();
-            getContentPane().add(pnlFiltroNombre, BorderLayout.NORTH);
-            {
-               lblNombre = new JLabel();
-               pnlFiltroNombre.add(lblNombre);
-               lblNombre.setText("Búsqueda incremental del nombre");
-            }
-            {
-               txtNombre = new JTextField();               
-               pnlFiltroNombre.add(txtNombre);
-               txtNombre.setColumns(30);
-               txtNombre.addCaretListener(new CaretListener() {
-                  public void caretUpdate(CaretEvent evt) {
-                     txtNombreCaretUpdate(evt);
-                  }
-               });
-            }
-         }
-         {  //se crea un JScrollPane para poder mostrar la tabla en su interior 
-            scrTabla = new JScrollPane();
-            getContentPane().add(scrTabla, BorderLayout.CENTER);
-            
-            
-          
-            // se define el modelo de la tabla donde se almacenarán las tuplas 
-            // extendiendo DefaultTableModel 
-            final class TablaEstacionadosModel extends DefaultTableModel{
-            	// define la clase java asociada a cada columna de la tabla
-       	        private Class[] types;
-         	    // define si una columna es editable
-                private boolean[] canEdit;
-                
-                TablaEstacionadosModel(){
-                	super(new String[][] {},
-                		  new String[]{"Calle", "Altura", "Id Parquimetro"});
-                	types = new Class[] {java.lang.String.class,
-                	                     java.lang.Integer.class, java.lang.String.class };
-                	canEdit= new boolean[] { false, false, false };
-                };             	
-            		             
-                // recupera la clase java de cada columna de la tabla
-                public Class getColumnClass(int columnIndex) 
-                {
-                   return types[columnIndex];
-                }
-                // determina si una celda es editable
-                public boolean isCellEditable(int rowIndex, int columnIndex) 
-                {
-                   return canEdit[columnIndex];
-                }         	          	            	
-            };
-            
-            TableModel EstacionadosModel = new TablaEstacionadosModel();
-                               
-            /*           
-             //Otra opción: definir el modelo de la tabla usando DefalutTableModel directamente 
-              TableModel BarcosModel =   
-                 new DefaultTableModel  
-                 (
-                    new String[][] {},
-                    new String[] {"Nombre", "Id", "Capitán"}
-                 );
-            */
-            
-               tabla = new JTable(); // Crea una tabla
-               tabla.setToolTipText("Doble-click o Espacio para seleccionar un parquimetro.");
-               tabla.addKeyListener(new KeyAdapter() {
-                  public void keyTyped(KeyEvent evt) {
-                     tablaKeyTyped(evt);
-                  }
-               });
-               
-               tabla.addMouseListener(new MouseAdapter() {
-                  public void mouseClicked(MouseEvent evt) {
-                     tablaMouseClicked(evt);
-                  }
-           		});
-               scrTabla.setViewportView(tabla); //setea la tabla dentro del JScrollPane srcTabla               
-               tabla.setModel(EstacionadosModel); // setea el modelo de la tabla  
-               tabla.setAutoCreateRowSorter(true); // activa el ordenamiento por columnas, para
-                                                   // que se ordene al hacer click en una columna
-            
-         }
-         pack();
       } catch (Exception e) {
          e.printStackTrace();
       }
@@ -249,5 +201,137 @@ public class VentanaInspector extends javax.swing.JInternalFrame
          System.out.println("VendorError: " + ex.getErrorCode());
       }
    }
+   
+   private void crearPanelTablaFiltro() {
+	       GridLayout thisLayout = new GridLayout(1,2);
+	       panelTablaFiltro= new JPanel(thisLayout);
+           
+           {  //se crea un JScrollPane para poder mostrar la tabla en su interior 
+               scrTabla = new JScrollPane();
+               panelTablaFiltro.add(scrTabla, BorderLayout.CENTER);
+             
+               // se define el modelo de la tabla donde se almacenarán las tuplas 
+               // extendiendo DefaultTableModel 
+               final class TablaEstacionadosModel extends DefaultTableModel{
+               	// define la clase java asociada a cada columna de la tabla
+          	        private Class[] types;
+            	    // define si una columna es editable
+                   private boolean[] canEdit;
+                   
+                   TablaEstacionadosModel(){
+                   	super(new String[][] {},
+                   		  new String[]{"Calle", "Altura", "Id Parquimetro"});
+                   	types = new Class[] {java.lang.String.class,
+                   	                     java.lang.Integer.class, java.lang.String.class };
+                   	canEdit= new boolean[] { false, false, false };
+                   };             	
+               		             
+                   // recupera la clase java de cada columna de la tabla
+                   public Class getColumnClass(int columnIndex) 
+                   {
+                      return types[columnIndex];
+                   }
+                   // determina si una celda es editable
+                   public boolean isCellEditable(int rowIndex, int columnIndex) 
+                   {
+                      return canEdit[columnIndex];
+                   }         	          	            	
+               };
+               
+               TableModel EstacionadosModel = new TablaEstacionadosModel();
+                                  
+               /*           
+                //Otra opción: definir el modelo de la tabla usando DefalutTableModel directamente 
+                 TableModel BarcosModel =   
+                    new DefaultTableModel  
+                    (
+                       new String[][] {},
+                       new String[] {"Nombre", "Id", "Capitán"}
+                    );
+               */
+               
+                  tabla = new JTable(); // Crea una tabla
+                  tabla.setToolTipText("Doble-click o Espacio para seleccionar un parquimetro.");
+                  tabla.addKeyListener(new KeyAdapter() {
+                     public void keyTyped(KeyEvent evt) {
+                        tablaKeyTyped(evt);
+                     }
+                  });
+                  
+                  tabla.addMouseListener(new MouseAdapter() {
+                     public void mouseClicked(MouseEvent evt) {
+                        tablaMouseClicked(evt);
+                     }
+              		});
+                  scrTabla.setViewportView(tabla); //setea la tabla dentro del JScrollPane srcTabla               
+                  tabla.setModel(EstacionadosModel); // setea el modelo de la tabla  
+                  tabla.setAutoCreateRowSorter(true); // activa el ordenamiento por columnas, para
+                                                      // que se ordene al hacer click en una columna
+               
+            }
+        //    pack();
+            getContentPane().add(panelTablaFiltro,BorderLayout.CENTER);
+   }
+   
+   private void crearPanelSeleccionados() {
+	   panelSeleccionados= new JPanel(new GridLayout(1,4));
+       panelSeleccionados.setPreferredSize(new Dimension(getContentPane().getWidth(),50));
+	   lblCalleSelec= new JLabel("Calle: ");
+	   lblAlturaSelec= new JLabel("Altura: ");
+	   lblIdParqSelec= new JLabel("Id parq: ");
+	   btnVerificar= new JButton("Verificar");
+	   btnVerificar.setMaximumSize(new Dimension(60,20));
+	   panelSeleccionados.add(lblCalleSelec);
+	   panelSeleccionados.add(lblAlturaSelec);
+	   panelSeleccionados.add(lblIdParqSelec);
+	   panelSeleccionados.add(btnVerificar);
+	   
+	   getContentPane().add(panelSeleccionados,BorderLayout.SOUTH);
+	   
+   }
 
+   private void crearPanelPatentes() { 
+	   panelPatentes= new JPanel(new BorderLayout());
+	   
+	   
+	   //-----------------------------------------------------
+	   //L1 TIENE QUE ESTAR EN UN SCROLLPANE
+	   //-----------------------------------------------------
+	   
+	   
+	   
+	   l1 = new DefaultListModel<String>();
+	   l1.addElement("Elemento de prueba");
+	   JList<String> list = new JList<String>(l1);  
+	   
+	   JPanel agregarPanel = new JPanel();
+	   JButton agregarPatenteBoton = new JButton();
+	   final JTextField patenteTextField = new JTextField(20);
+	   
+	   agregarPatenteBoton.setText("Agregar");
+	   agregarPatenteBoton.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			l1.addElement(patenteTextField.getText().toString());
+			patenteTextField.setText("");
+		}
+		   
+	   });
+	   
+	   agregarPanel.setLayout(new GridLayout(1,0));
+	   agregarPanel.add(patenteTextField);
+	   agregarPanel.add(agregarPatenteBoton);
+	   
+	   JLabel tituloLabel = new JLabel("Patentes:");
+	   tituloLabel.setFont(new Font("Arial", Font.PLAIN, 22));
+	   tituloLabel.setAlignmentX(CENTER_ALIGNMENT);
+	   
+	   panelPatentes.add(tituloLabel,BorderLayout.NORTH);
+	   panelPatentes.add(list,BorderLayout.CENTER);
+	   panelPatentes.add(agregarPanel,BorderLayout.SOUTH);
+	   panelTablaFiltro.add(panelPatentes);
+	   
+	   
+   }
 }
